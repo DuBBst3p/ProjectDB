82360adb00052585ff960e8e62ebd912de38e78f
yAl3x <153630778+yAl3x@users.noreply.github.com>
2025-05-05 20:50:37 +0300

    Push

--- ProjectDB/obj/Release/net9.0/ProjectDB.AssemblyInfo.cs
+++ ProjectDB/obj/Release/net9.0/ProjectDB.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectDB")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fac066928ab278fda75942314a16881d3afa9734")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectDB")]
